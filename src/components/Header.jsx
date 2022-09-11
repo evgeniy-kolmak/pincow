756fddbd77f92dd806a3bdffa5e2b55edc64f564
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-09-11 12:25:07 +0300

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Box } from '@mui/material';
 import { useDate } from '../hook/date';
 import { usePosition } from '../hook/positionWeather';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Menu, Phishing } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+
 
 
 export default function Header({ handleMenu }) {
@@ -11,21 +12,60 @@ export default function Header({ handleMenu }) {
 
   return (
     <AppBar position='static'>
-      <Toolbar>
-        <Link to="/">
-          <img style={{ width: '150px', marginRight: '60%' }} src="./images/pincow-logo.png" alt="" />
-        </Link>
-        <Typography variant='h6' component='span' sx={{ flexGrow: 1, ml: '65%' }}>Сегодня {currentDateString} </Typography>
-        <Typography variant='h6' component='span' sx={{ flexGrow: 0.1 }}>
-          {cityName} {temp} &deg;
-          <img style={{ width: '50px', verticalAlign: 'middle', marginLeft: '10px' }} src={`https://openweathermap.org/img/wn/${iconId}@2x.png`} />
-        </Typography>
+      <Toolbar >
+        <Box sx={{ flexGrow: 1, }}>
+          <Link style={{ color: 'inherit', textDecoration: 'none', display: 'flex' }} to="/">
+            <Phishing sx={{
+              color: '#f2df33'
+            }} fontSize="large" />
+            <Typography
+              variant="h5"
+              component="span"
+              sx={{
+                ml: 1,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.4rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                textTransform: 'uppercase',
+                color: '#ede8e8',
+              }}
+            >
+              pincow
+            </Typography>
+          </Link>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: '17px'
+            }}
+          >Сегодня {currentDateString} </Typography>
+          <Typography
+            variant="h6"
+            component="span"
+            sx={{
+              ml: 4,
+            }}>
+            {cityName} {temp} &deg;
+            {/* change icon */}
+            <img style={{ width: '50px', verticalAlign: 'middle', marginLeft: '5px' }} src={`https://openweathermap.org/img/wn/${iconId}@2x.png`} />
+          </Typography>
+        </Box>
         <IconButton onClick={handleMenu}>
-          <MenuIcon
-            sx={{ color: '#fff' }} />
+          <Menu fontSize='large' sx={{ ml: 5, color: "#fff" }} />
         </IconButton>
       </Toolbar>
-    </AppBar>
+    </AppBar >
 
   );
 
