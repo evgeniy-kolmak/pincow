c7f5b9f126ddf33c5b5a1c75c57b4fd17d41950b
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-10-22 18:34:15 +0300

    add visual curent forecast

--- src/pages/forecast/Current.jsx
+++ src/pages/forecast/Current.jsx
@@ -1,41 +1,78 @@
 import { useWeather } from "../../hook/weather";
-import { Typography, Icon, List, ListItem, ListItemIcon } from '@mui/material';
-import { North, Air, Compress, WbTwilight, WbSunny } from '@mui/icons-material';
+import { Typography, Icon, List, ListItem, Tooltip, ListItemText, Box } from '@mui/material';
+import { North, Air, Compress, WbTwilight, WbSunny, People, FilterDrama, Opacity, Grain, FormatColorReset, Visibility, FiberManualRecord } from '@mui/icons-material';
 export default function Current({ data }) {
   const { city, base, wind, main } = useWeather(data, 'current');
   return (
     <>
       <Typography variant='h2' >Текущий прогноз</Typography>
       <List>
-        <ListItem>
-          <Typography variant='h3' component='span'>{city.name} | {city.country} {base.temp}&deg;</Typography>
-          <Icon sx={{ fontSize: 40 }}>
-            <img src={`../images/icons/${base.iconId}.svg`} />
-          </Icon>
+        <ListItem sx={{ display: 'block' }}>
+          <Typography sx={{ mb: 0.5 }} variant='h3' component='h6'>{city.name} | {city.country}</Typography>
+          <Tooltip title="Население" placement="bottom-start">
+            <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+              <People sx={{ mr: 0.5, fontSize: 26 }} />{city.population} человек
+            </Typography>
+          </Tooltip>
         </ListItem>
+        <Box sx={{ border: '0.15rem solid #ccc', borderRadius: '18px', width: 'max-content' }}>
+          <ListItem >
+            <Icon sx={{ fontSize: 90, mr: 1 }}>
+              <img src={`../images/icons/${base.iconId}.svg`} />
+            </Icon>
+            <ListItemText primary={<Typography variant='h4' component='h6'>{base.description}</Typography>} secondary={
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant='h4' component='span'>{base.temp}&deg;</Typography>
+                <Typography>(Ощущается как: {main.tempFeels}&deg;)</Typography>
+              </div>
+            } />
+          </ListItem>
+          <ListItem sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly' }}>
+            <Tooltip title="Ветер" placement="bottom-start">
+              <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+                <Air sx={{ mr: 0.5, fontSize: 30 }} />{wind.direction} <North style={{ transform: `rotate(${wind.deg}deg)` }} />{wind.speed}м/c
+              </Typography>
+            </Tooltip>
+            <FiberManualRecord sx={{ ml: 1.5, mr: 1.5, fontSize: 12, color: '#ccc' }} />
+            <Tooltip title="Давление" placement="bottom-start">
+              <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+                <Compress sx={{ mr: 0.5, fontSize: 30 }} />{main.pressure} мм рт. ст.
+              </Typography>
+            </Tooltip>
+            <FiberManualRecord sx={{ ml: 1.5, mr: 1.5, fontSize: 12, color: '#ccc' }} />
+            <Tooltip title="Видимость" placement="bottom-start">
+              <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+                <Visibility sx={{ mr: 0.5, fontSize: 30 }} />{main.visibility}км
+              </Typography>
+            </Tooltip>
+          </ListItem>
+        </Box>
+        <ListItem> <Typography sx={{ display: 'flex', fontSize: 17 }}><WbSunny sx={{ mr: 0.5, fontSize: 26 }} /> Восход: {city.sunrise} /  <WbTwilight sx={{ mr: 0.5, fontSize: 26 }} /> Закат: {city.sunset}</Typography></ListItem>
         <ListItem>
-          <Typography variant='h5' component='h6'>{base.description}</Typography>
+          <Tooltip title="Облачность" placement="bottom-start">
+            <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+              <FilterDrama sx={{ ml: 0.7, mr: 0.5, fontSize: 30 }} />{main.clouds}%
+            </Typography>
+          </Tooltip>
+          <Tooltip title="Влажность" placement="bottom-start">
+            <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+              <Opacity sx={{ ml: 0.7, mr: 0.5, fontSize: 30 }} />{main.humidity}%
+            </Typography>
+          </Tooltip>
+          <Tooltip title="Влероятность осадков" placement="bottom-start">
+            <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+              <FormatColorReset sx={{ ml: 0.7, mr: 0.5, fontSize: 30 }} />{main.pop}%
+            </Typography>
+          </Tooltip>
+          <Tooltip title="Количество осадков за 3 часа" placement="bottom-start">
+            <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 17 }}>
+              <Grain sx={{ ml: 0.7, mr: 0.5, fontSize: 30 }} />{main.count} мм
+            </Typography>
+          </Tooltip>
         </ListItem>
-        <Typography>Население: {city.population} человек</Typography>
-        <Typography>Облачность: {main.clouds}%</Typography>
-        <Typography>Ощущается как {main.tempFeels}&deg;</Typography>
-        <Typography>Вероятность осадков {main.pop}%</Typography>
-        <Typography>Влажность {main.humidity}%</Typography>
-        <Typography>Количество осадков {main.count} мм/за 3 часа</Typography>
-        <Typography><WbSunny /> Восход: {city.sunrise} /  <WbTwilight /> Закат: {city.sunset}</Typography>
-        <ListItem>
-          <ListItemIcon >
-            <Air fontSize='large' />
-          </ListItemIcon>
-          <Typography style={{ display: 'flex', alignItems: 'center' }} variant='h6' component='span'>Ветер: {wind.direction}<North style={{ transform: `rotate(${wind.deg}deg)` }} />{wind.speed}м/c</Typography>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon >
-            <Compress fontSize='large' />
-          </ListItemIcon>
-          <Typography variant='h6' component='span'>Давление: {main.pressure} мм рт. ст.</Typography>
-        </ListItem>
+
       </List>
+
     </>
   );
 
