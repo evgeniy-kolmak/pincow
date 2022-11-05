500ae4bc706033f5a11c62c664ff8acf459205de
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-11-05 09:41:13 +0300

    fix content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
-import { Card, Box, Button, Typography, List, ListItem } from '@mui/material';
+import { Card, Box, Button, Typography } from '@mui/material';
 import { FormatQuote, Clear } from '@mui/icons-material';
-export default function Error({ data }) {
-  console.log(data);
 
+export default function Error({ data }) {
   const city = data;
+
   return (
     <Box sx={{
       display: 'flex',
@@ -17,22 +17,20 @@ export default function Error({ data }) {
         }}
       >
         <Typography sx={{ display: 'flex', alignItems: 'center' }} variant='h1'>Упс!<Clear color="error" sx={{ fontSize: 100 }} /></Typography>
-
         <Typography variant='h5'>Что-то пошло не так. Возможно где-то опечатка.</Typography>
-        <Typography variant='h5' sx={{ display: 'flex', alignItems: 'center', fontSize: 28, }}>Вы искали:
+        <Typography variant='h5' sx={{ display: 'flex', alignItems: 'center', fontSize: 28, mt: 0.4, mb: 1 }}>Вы искали:
           <FormatQuote sx={{ alignSelf: 'end', ml: 1, mr: 0.4 }} />
           <Typography sx={{ color: '#2b5c8a', textDecoration: 'underline', fontSize: 33, cursor: 'pointer' }}><a href={`http://google.com/search?q=${city}`} target="_blank">{city}</a></Typography>
           <FormatQuote sx={{ alignSelf: 'start', ml: 0.2 }} /></Typography>
-        <List>
-          <ListItem>
-
-          </ListItem>
-        </List>
+        <Typography variant='h6'>Проверьте правильность введеных данных и попробуйте еще раз.</Typography>
       </Card>
 
       <Button
         variant="contained"
-        sx={{ width: 'max-content' }}
+        sx={{
+          width: 'max-content',
+          mt: 4,
+        }}
         type='submit'>
         <Link
           style={{
