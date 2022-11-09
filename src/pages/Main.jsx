4d054eff3bc97ca8ccd05e186bc84e54d5a004ce
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-11-09 20:11:55 +0300

    add map  & start text

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,65 +1,54 @@
-import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import Loader from "../components/Loader";
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import { Box, Typography } from '@mui/material';
-import { Menu, WrongLocation } from '@mui/icons-material';
-
+import Map from "../components/Map";
+import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { Menu, Layers, DoNotTouch } from '@mui/icons-material';
+import { useDate } from "../hook/date";
 
 export default function Main() {
-  const [isPageLoading, setIsPageLoading] = useState(true);
-  const [coords, setCoords] = useState(null);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      setCoords([lat, lon]);
-      coords ? setIsPageLoading(true) : setIsPageLoading(false);
-    })
-  }, [])
-
-  const markerIcon = new L.Icon({
-    iconUrl: '/images/location.svg',
-    iconSize: [50, 65],
-    popupAnchor: [0, -25]
-  });
-
+  const { greeting } = useDate()
   return (
     <Box
       sx={{
         bgcolor: '#fff',
-        height: '100%',
-        width: '100%'
       }}
     >
-      {!isPageLoading ? <MapContainer style={{ height: "450px", width: "100%" }} center={coords} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={coords} icon={markerIcon}>
-          <Popup>
-            Вы здесь!
-          </Popup>
-        </Marker>
-      </MapContainer > : <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '380px',
-          width: '100%',
-          background: 'linear-gradient(to right, #2b5876, #4e4376)',
-          backgroundSize: '400%, 400%',
-          animation: 'gradient 40s linear 0s infinite normal forwards',
-          flexDirection: 'column',
-        }}>
-        <Loader />
-        <Typography variant="h5" sx={{ display: 'flex', alignItems: 'center', color: '#fff', mt: 2, }}><WrongLocation sx={{ mr: 0.8, fontSize: 28 }} /> Поиск геопозиции</Typography>
-        <Typography sx={{ color: '#fff', mt: 0.65, fontSize: 15 }}>Разрешите доступ к геоданным и перезагрузите страницу</Typography>
-      </Box>}
+      <List>
+        <ListItem>
+          <ListItemText
+            primary={<Typography variant='h2' component='p'>{greeting}, дорогой друг!</Typography>}
+            secondary={<Typography variant='h6' component='p'>Очень рады, что вы заглянули к нам.</Typography>} />
+        </ListItem>
+        <ListItem>
+          <Typography
+            sx={{
+              bgcolor: '#fcad03',
+              p: 0.5,
+              m: 0.5,
+              borderRadius: 1.2,
+              fontSize: 30,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.2rem',
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+              color: 'inherit',
+            }}>pincow</Typography>
+          <Typography fontSize={18}> = небольшое приложение, предоставляющие данные о погоде.</Typography>
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary={<Typography variant='h6' component='p'>Ниже карта с твоим местоположением - кординаты нужны для определения текущего прогноза.</Typography>}
+            secondary={<Typography color="#ccc">По желанию,  вы можете запретить его использование.</Typography>}
+          />
+        </ListItem>
+      </List>
+      <Map size={['100%', '380px']} />
+
+      <Typography color="error" sx={{ display: 'flex', alignItems: 'center', pl: '3%', mt: 0.6, fontSize: 14 }}> <DoNotTouch sx={{ mr: 0.4 }} fontSize="small" color='error' />Примечание: Мы не отслеживаем и не храним данные.</Typography>
+      <List>
+        <ListItem>
+          <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 18 }} >Для дальнейшего взаимодествия: Открой меню <Menu fontSize="large" sx={{ ml: 0.3, mr: 0.3 }} /> и выбери <Layers fontSize="large" sx={{ ml: 0.3, mr: 0.3 }} /> "Прогноз".</Typography>
+        </ListItem>
+      </List>
     </Box>
 
   );
