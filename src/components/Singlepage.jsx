04954d2d5c12f2f79fa1b0c10347f88086886e5f
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-01-15 18:46:35 +0300

    fixed height page

--- src/components/Singlepage.jsx
+++ src/components/Singlepage.jsx
@@ -5,13 +5,14 @@ import Sidebar from './Sidebar';
 import Header from './Header';
 import { usePosition } from '../hook/positionWeather';
 import { useBackground } from '../hook/background';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
 export default function SinglePage({ data }) {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const { iconId } = usePosition();
   const color = useBackground(data, iconId);
+  const matches = useMediaQuery('@media (max-width:600px)');
 
   return (
     <div
@@ -19,24 +20,25 @@ export default function SinglePage({ data }) {
         backgroundImage: color,
         backgroundSize: '400%, 400%',
         animation: 'gradient 40s linear 0s infinite normal forwards',
-        height: '100vh'
+        minHeight: '100vh'
       }}
     >
       <Header handleMenu={() => setMenuOpen(true)} />
       <Container
-        style={{
+        fixed
+        sx={{
           backgroundColor: '#1E1E1E',
-          height: 'calc(100vh - 64px)',
+          minHeight: `calc(100vh - ${matches ? 56 : 64}px)`,
           padding: '40px',
         }}
       >
-        <Sidebar
-          menuOpen={isMenuOpen}
-          closeMenu={() => setMenuOpen(false)}
-        />
         <Outlet />
       </Container>
-    </div>
+      <Sidebar
+        menuOpen={isMenuOpen}
+        closeMenu={() => setMenuOpen(false)}
+      />
+    </div >
   );
 }
 
