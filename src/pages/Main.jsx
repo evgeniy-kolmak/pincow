086afe1abb66be24ab1c184d8d34cba58abbf354
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-01-15 15:08:24 +0300

    responsive page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import Map from "../components/Map";
-import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { Box, Typography, List, ListItem, ListItemText, Grid } from '@mui/material';
 import { Menu, Layers, DoNotTouch } from '@mui/icons-material';
 import { useDate } from "../hook/date";
 
@@ -15,25 +15,35 @@ export default function Main() {
       <List>
         <ListItem>
           <ListItemText
-            primary={<Typography variant='h2' component='p'>{greeting}, дорогой друг!</Typography>}
+            primary={<Typography variant='h2' component='p' >{greeting}, дорогой друг!</Typography>}
             secondary={<Typography variant='h6' component='p'>Очень рады, что вы заглянули к нам.</Typography>} />
         </ListItem>
         <ListItem>
-          <Typography
-            sx={{
-              bgcolor: '#fcad03',
-              p: 0.5,
-              m: 0.5,
-              borderRadius: 1.2,
-              fontSize: 30,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.2rem',
-              textDecoration: 'none',
-              textTransform: 'uppercase',
-              color: 'inherit',
-            }}>pincow</Typography>
-          <Typography fontSize={18}> = небольшое приложение, предоставляющие данные о погоде.</Typography>
+          <Grid container
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            <Grid item md={2} sm={3.5} xs={6}>
+              <Typography
+                align="center"
+                sx={{
+                  bgcolor: '#fcad03',
+                  p: 0.5,
+                  mr: 1,
+                  mb: 1,
+                  borderRadius: 1.2,
+                  fontSize: 30,
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.2rem',
+                  textDecoration: 'none',
+                  textTransform: 'uppercase',
+                  color: 'inherit',
+                }}>pincow</Typography>
+            </Grid>
+            <Grid item md={8} sm={8} xs={12}>
+              <Typography fontSize={16}>Небольшое приложение, предоставляющие данные о погоде.</Typography>
+            </Grid>
+          </Grid>
         </ListItem>
         <ListItem>
           <ListItemText
@@ -41,16 +51,24 @@ export default function Main() {
             secondary={<Typography color="#ccc">По желанию,  вы можете запретить его использование.</Typography>}
           />
         </ListItem>
-      </List>
-      <Map />
-
-      <Typography color="error" sx={{ display: 'flex', alignItems: 'center', pl: '3%', mt: 0.6, fontSize: 14 }}> <DoNotTouch sx={{ mr: 0.4 }} fontSize="small" color='error' />Примечание: Мы не отслеживаем и не храним данные.</Typography>
-      <List>
+        <Map />
+        <Typography color="error" sx={{ display: 'flex', alignItems: 'center', pl: '3%', mt: 0.6, fontSize: 14 }}> <DoNotTouch sx={{ mr: 0.4 }} fontSize="small" color='error' />Примечание: Мы не отслеживаем и не храним данные.</Typography>
         <ListItem>
-          <Typography sx={{ display: 'flex', alignItems: 'center', fontSize: 18 }} >Для дальнейшего взаимодействия: Открой меню <Menu fontSize="large" sx={{ ml: 0.3, mr: 0.3 }} /> и выбери <Layers fontSize="large" sx={{ ml: 0.3, mr: 0.3 }} /> "Прогноз".</Typography>
+          <Grid container
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            <Typography sx={{ mr: 1 }} variant="h6" component="p">Для дальнейшего взаимодействия:</Typography >
+            <Grid sx={{ display: 'flex', alignItems: 'center' }} item lg={2} md={2.5} sm={4.5} xs={12}>
+              <Typography>Откройте меню</Typography><Menu fontSize="large" sx={{ ml: 0.5, mr: 0.5 }} />
+            </Grid>
+            <Grid sx={{ display: 'flex', alignItems: 'center' }} item md={4} sm={6} xs={12}>
+              <Typography> и выберите "Прогноз"</Typography><Layers fontSize="large" sx={{ ml: 0.5, mr: 0.5 }} />
+            </Grid>
+          </Grid>
         </ListItem>
       </List>
-    </Box>
+
+    </Box >
 
   );
 }
