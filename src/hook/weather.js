08c14478a876730f9771edcc1852bae88ae9ee47
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-01-23 14:10:43 +0300

    responsive page forecast 5 days

--- src/hook/weather.js
+++ src/hook/weather.js
@@ -32,16 +32,67 @@ export function useWeather(data, forecast) {
 
   const getJsxWeek = getIcon.map((item, i) => (
     <Box key={i} >
-      <Box sx={{ display: 'flex', alignItems: 'center', mt: 1.5, ml: 3.5 }}>
-        <Typography sx={{ width: 32, fontSize: 27 }} variant='h5' component='p'>{dayWeek[i]}:</Typography>
-        <Icon sx={{ fontSize: 60, mr: 3, ml: 3 }}>
-          <img src={`../images/icons/${item}d.svg`} />
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        p: {
+          md: '1.5rem 0 0 3.5rem ',
+          sm: '1rem 0 0 2rem ',
+          xs: '0.5rem 0 0 0.8rem '
+        }
+
+      }}>
+        <Typography sx={{
+          width: 32,
+          fontSize: 27
+        }}
+          variant='h5'
+          component='p'>
+          {dayWeek[i]}:
+        </Typography>
+        <Icon
+          sx={{
+            fontSize: {
+              lg: 60,
+              md: 55,
+              sm: 47,
+              xs: 40
+            },
+            m: {
+              md: '0 3rem',
+              sm: '0 2rem',
+              xs: '0 1rem'
+            }
+          }}>
+          <img src={`../images/icons/${item}d.svg`} alt='' />
         </Icon>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant='h5' component='span'>{getTemp[i].maxTemp}&deg;</Typography> <Commit sx={{ mr: 0.6, ml: 0.6 }} /> <Typography variant='h5' component='span'>{getTemp[i].minTemp}&deg;</Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+          <Typography
+            variant='h5'
+            component='span'>
+            {getTemp[i].maxTemp}&deg;
+          </Typography>
+          <Commit
+            sx={{
+              mr: 0.6,
+              ml: 0.6
+            }} />
+          <Typography
+            variant='h5'
+            component='span'
+          >
+            {getTemp[i].minTemp}&deg;
+          </Typography>
         </Box>
       </Box>
-      <Divider sx={{ mt: 0.5 }} />
+      <Divider
+        sx={{
+          mt: 0.5
+        }} />
     </Box >
   ));
 
--- src/pages/forecast/Week.jsx
+++ src/pages/forecast/Week.jsx
@@ -1,14 +1,23 @@
 import { useWeather } from "../../hook/weather";
-import { Typography } from '@mui/material';
+import { Typography, Grid, Box } from '@mui/material';
 export default function Week({ data }) {
   const forecastWeek = useWeather(data, 'week');
   const city = data.city.name;
 
   return (
-    <>
-      <Typography variant='h2' >{city}: Прогноз на 5 дней  </Typography>
+    <Box>
+      <Grid container spacing={1}>
+        <Grid item>
+          <Typography variant='h3' component='p'><span style={{ fontWeight: 500 }}>{city}:</span></Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant='h3' component='p'>
+            Прогноз на 5 дней
+          </Typography>
+        </Grid>
+      </Grid>
       {forecastWeek}
-    </>
+    </Box>
   );
 
 }
