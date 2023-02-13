66e0fcd1d647571cbdebd27b195a44b5e3039869
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2023-02-13 23:21:14 +0300

    responsive page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,17 @@
-import { Box, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar, Button } from '@mui/material';
-import { Person, Language, Insights, Map, East, KeyboardBackspace, ZoomInMap, Filter1, Filter5, People, Compress, Visibility, Opacity } from '@mui/icons-material';
+import { Box, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar, Button, Grid } from '@mui/material';
+import {
+  Person,
+  Language,
+  Insights,
+  Map,
+  People,
+  Compress,
+  Visibility,
+  Opacity,
+  DateRange,
+  Today,
+  CalendarMonth
+} from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 
@@ -15,91 +27,170 @@ export default function About() {
           <Typography variant='h2' component='p'>Как это работает?</Typography>
         </ListItem>
         <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <Person />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Вы" secondary="Пользователь" />
-          <East fontSize='large' sx={{ mr: 3 }} />
-          <ListItemAvatar>
-            <Avatar>
-              <Language />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Заходите на сайт" secondary="PINCOW" />
-          <East fontSize='large' sx={{ mr: 3 }} />
-          <ListItemAvatar>
-            <Avatar>
-              <Map />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Используете карту" secondary="Или ручной ввод" />
-          <East fontSize='large' sx={{ mr: 3 }} />
-          <ListItemAvatar>
-            <Avatar>
-              <Insights />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Результат" secondary="Получаете текущий прогноз" />
-        </ListItem>
+          <Grid container spacing={4}>
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                < Avatar >
+                  <Person />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Вы" secondary="Пользователь" />
+            </Grid>
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Language />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Заходите на сайт" secondary="PINCOW" />
+            </Grid>
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Map />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Используете карту" secondary="Или ручной ввод" />
+            </Grid>
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Insights />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Результат" secondary="Получаете текущий прогноз" />
+            </Grid>
+          </Grid>
+        </ListItem >
         <ListItem>
           <ListItemText
-            primary={<Typography variant='h4' component='p'>Давайте более детально.</Typography>}
-            secondary={<Typography sx={{ fontSize: 18 }}>На каждой странице есть подсказки. (Так же всплывающие "При наведении")</Typography>} />
-
-        </ListItem>
-        <ListItem>
-          <ListItemText
-            primary={<Typography variant='h6' component='p'>При успешной отправке, вам отобразится текущий прогноз.</Typography>}
-            secondary={<Typography sx={{ display: 'flex', alignItems: 'center' }}>
-              <People sx={{ mr: 0.5 }} /> Население
-              <Visibility sx={{ ml: 1, mr: 0.5 }} /> Видимость
-              <Compress sx={{ ml: 1, mr: 0.5 }} /> Давление
-              <Opacity sx={{ ml: 1, mr: 0.5 }} /> Влажность и т.д. </Typography>} />
-        </ListItem>
-        <ListItem>
-          <ListItemText
-            primary={<Typography variant='h6' component='p'>Меню управления прогнозом</Typography>}
-            secondary={
-              <Typography sx={{ display: 'flex' }}><KeyboardBackspace
-                sx={{
-                  bgcolor: '#1F1F28',
-                  color: '#fff',
-                  borderRadius: '50%',
-                  width: 45,
-                  height: 45,
-                  p: 0.5,
-                  mt: 1,
+            primary={<Typography sx={{
+              fontSize: {
+                md: 34,
+                sm: 30,
+                xs: 26
+              },
+              fontWeight: 400,
+              mb: {
+                md: 1,
+                sm: 0.7,
+                xs: 0.5
+              }
+            }}>Давайте более детально.</Typography>}
+            secondary={<Typography
+              sx={{
+                fontSize: {
+                  md: 18,
+                  sm: 16.5,
+                  xs: 15
                 }
-                } />
-              </Typography>}
+              }}>На каждой странице есть подсказки. (Так же всплывающие "При наведении")</Typography>} />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary={<Typography variant='h6' component='p'
+              sx={{
+                mb: {
+                  md: 1.8,
+                  sm: 2.3,
+                  xs: 2.6
+                }
+              }}>При успешной отправке, вам отобразится текущий прогноз.</Typography>}
+            secondary={
+              <Grid
+                container
+                spacing={3.5}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center'
+                }}
+              >
+                <Grid item>
+                  <Typography> <People sx={{ mr: 0.5, color: '#000', verticalAlign: 'middle' }} /> Население</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography><Visibility sx={{ mr: 0.5, color: '#000', verticalAlign: 'middle' }} /> Видимость</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography><Compress sx={{ mr: 0.5, color: '#000', verticalAlign: 'middle' }} /> Давление</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography><Opacity sx={{ mr: 0.5, color: '#000', verticalAlign: 'middle' }} /> Влажность и т.д.</Typography>
+                </Grid>
+              </Grid>
+            } />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary={<Typography variant='h6' component='p'>Меню управления страницами прогноза:</Typography>}
           />
         </ListItem>
         <ListItem>
-          <Typography variant='h6' component='p'>Пункты меню:</Typography>
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Today />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Текущий" secondary="В данный момент" />
+            </Grid>    <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <DateRange />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="На сутки" secondary="24 часа" />
+            </Grid>
+            <Grid
+              item
+              sx={{
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+              <ListItemAvatar>
+                <Avatar>
+                  <CalendarMonth />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="На 5 дней" secondary="Среднее значение в день" />
+            </Grid>
+          </Grid>
         </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <ZoomInMap />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Текущий" secondary="В данный момент" />
-          <ListItemAvatar>
-            <Avatar>
-              <Filter1 />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="На сутки" secondary="24 часа" />
-          <ListItemAvatar>
-            <Avatar>
-              <Filter5 />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="На 5 дней" secondary="Среднее значение в день" />
-        </ListItem>
-      </List>
+      </List >
       <Button sx={{ ml: 5, mb: 5, mt: 3 }} variant="contained"><Link
         style={{
           color: 'inherit',
